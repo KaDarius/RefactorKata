2e8a2c01fe2f483da9ee3e01b89b64397501b756
KaDarius King <kdar09@gmail.com>
2017-10-11 23:38:44 -0500

    added my reader and products variable with while statement completing the List for Product

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -30,8 +30,18 @@ namespace RefactorKata
                 cmd.CommandText = "Select * from Invoices";
             }
 
+            /*creating while loop to link my products var with my Product list, I believe to link the foreach statement? maybe*/
+
+            var reader = cmd.ExecuteReader();
+            var products = new List<Product>();
+
+            while (reader.Read())
+            {
+                
+            }
+
             /*
-             * lost myself on IEnum, following along with c repo to explain what I do understand line 52
+             * lost myself on IEnum, following along with c repo to explain what I do understand past IEnum
             var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
             /*
