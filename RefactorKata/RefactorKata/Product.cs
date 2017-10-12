b269f2f68fb6f182948baca9eec0b840a01030ca
KaDarius King <kdar09@gmail.com>
2017-10-11 21:24:24 -0500

    added a Product project for the while loop to help with CCR

---
+++ RefactorKata/RefactorKata/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Data.SqlClient;
+
+namespace RefactorKata
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var products = GetProducts();
+            foreach (var products in products)
+            {
+                Console.WriteLine ("This product is called: " + product.NAme);
+            }
+        }
+
+        private static IEnumerable<Product> GetProducts()
+        {
+            using (var conn =new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
+            
+            {
+
+            }
+        }
+    }
+}
